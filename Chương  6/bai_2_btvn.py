83712389101cdace558bbaaa1111039dd7870eb7
Nguyen_Duc_Hieu <ndhieu.24174600102@sv.uneti.edu.vn>
2024-10-24 10:11:17 +0700

    làm bài tập 2

---
+++ Chương  6/bai_2_btvn.py
@@ -0,0 +1,10 @@
+import math
+
+x = float(input("Nhập giá trị x: "))
+if x**2 + 4 >= 0 and x**4 + 1 > 0:
+    numerator = -x + math.sqrt(x**2 + 4)
+    denominator = (x**4 + 1)**(1/7)
+    f_x = numerator / denominator
+    print(f"Giá trị của f(x) là: {f_x:.2f}")
+else:
+    print("Biểu thức không xác định được với giá trị x đã nhập.")
