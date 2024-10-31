e33102607d9bd6286d82e587b000bef21000a3ae
Nguyen_Duc_Hieu <ndhieu.24174600102@sv.uneti.edu.vn>
2024-10-31 08:33:24 +0700

    làm bài 6

---
+++ Chương 8/bai_6.py
@@ -0,0 +1,13 @@
+print("1. Phim tình cảm\n2. Phim kinh dị\n3. Phim hoạt hình\n4. Phim khoa học viễn tưởng")
+chon = int(input("Chọn thể loại phim (1-4): "))
+
+if chon == 1:
+    print("Phim tình cảm")
+elif chon == 2:
+    print("Phim kinh dị")
+elif chon == 3:
+    print("Phim hoạt hình")
+elif chon == 4:
+    print("Phim khoa học viễn tưởng")
+else:
+    print("Lựa chọn không hợp lệ")
